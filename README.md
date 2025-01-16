40823b3c51d84b9d17c84f8e810e8725bbfceefa
TjRichmond <tjrichmond99@gmail.com>
2025-01-15 20:08:52 -0800

    Created Rust blink rgb led for sparkfun board

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Magic Dial
 
 
-## Components
+## Crates
 | Name | Description | Crate |
 | - | - | - |
 | HX711 | Strain Gauge IC | https://crates.io/crates/hx711 |
 | GC9A01 | LCD Screen | https://crates.io/crates/gc9a01-rs |
+| MT6701 | Hall Encoder | https://crates.io/crates/mt6701 |
+| TMC6300 | Motor Driver | TBD |
+| INA420 | Inline Phase Current | TBD |
+
+Sparkfun Board | https://docs.sparkfun.com/SparkFun_IoT_Brushless_Motor_Driver/introduction/
+TMC6300 Motor Driver
+TMAG5273 Hall Encoder
+INA420 Inline Phase Current
+MCP6021 Low-side Op Amp
+WS2812 RGB LED | https://crates.io/crates/ws2812-spi
--- firmware/src/bin/main.rs
+++ firmware/src/bin/main.rs
@@ -2,25 +2,48 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::delay::Delay;
-use esp_hal::prelude::*;
+use esp_hal::{
+    delay::Delay,
+    prelude::*,
+    spi::{
+        master::{Spi, Config},
+        SpiMode}
+};
 use log::info;
+use ws2812_spi::Ws2812;
+use smart_leds::{SmartLedsWrite, RGB8};
 
 #[entry]
 fn main() -> ! {
-    let _peripherals = esp_hal::init({
+    let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::max();
         config
     });
 
     esp_println::logger::init_logger_from_env();
-
     let delay = Delay::new();
+
+    let mosi = peripherals.GPIO2;
+    let rgb_spi = Spi::new_with_config(
+        peripherals.SPI2,
+        Config {
+            frequency: 3u32.MHz(),
+            mode: SpiMode::Mode0,
+            ..Config::default()
+        },
+    ).with_mosi(mosi);
+
+    let mut rgb_led = Ws2812::new(rgb_spi);
+    let on_led: [RGB8; 1] = [RGB8{r:0x10, g:0x10, b:0x10}];
+    let off_led: [RGB8; 1] = [RGB8{r:0, g:0, b:0}];
+
     loop {
-        info!("Hello world!");
+        info!("On");
+        rgb_led.write(on_led.iter().cloned()).unwrap();
+        delay.delay(500.millis());
+        info!("Off");
+        rgb_led.write(off_led.iter().cloned()).unwrap();
         delay.delay(500.millis());
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
 }
